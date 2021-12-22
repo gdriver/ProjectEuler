2ae80aaf6c558977e1c35ce1f19a556a27dd828c
Gareth Driver <gareth.driver@gmail.com>
2021-12-22 20:31:03 +0000

    small changes

--- Main.hs
+++ Main.hs
@@ -2,15 +2,17 @@ import Problem1
 --import Problem2
 --import Problem3
 import Problem4
-import Problem5
+--import Problem5
 import Problem6
 import Problem7
+import Problem8
 
 main = do
   p1
 --p2 problem 2 solution is not very efficient; see that file for details
 --p3
   p4
-  p5
+ -- p5
   p6
   p7
+  p8

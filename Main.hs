1be697fe502b9d366ed5c3ea26240194839c1038
Gareth Driver <gareth.driver@gmail.com>
2021-12-22 21:04:43 +0000

    start problem 9

--- Main.hs
+++ Main.hs
@@ -1,18 +1,20 @@
-import Problem1
+---import Problem1
 --import Problem2
 --import Problem3
-import Problem4
+--import Problem4
 --import Problem5
-import Problem6
-import Problem7
-import Problem8
+--import Problem6
+--import Problem7
+--import Problem8
+import Problem9
 
 main = do
-  p1
---p2 problem 2 solution is not very efficient; see that file for details
---p3
-  p4
- -- p5
-  p6
-  p7
-  p8
+  -- p1
+  -- p2 problem 2 solution is not very efficient; see that file for details
+  -- p3
+  -- p4
+  -- p5
+  -- p6
+  -- p7
+  -- p8
+  p9
